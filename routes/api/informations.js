fc1525c00e9b6876a30c43945c0b2dbde46462fe
Abeer Ahmed <abeerahmad389@gmail.com>
2022-05-25 01:37:51 +0500

    bugs removed

--- routes/api/informations.js
+++ routes/api/informations.js
@@ -45,50 +45,58 @@ router.delete("/:id", async (req, res) => {
   let information = await Information.findByIdAndDelete(req.params.id);
   return res.send(information);
 });
-//Insert a record
-router.post(
-  "/",
-  upload.single("cardphoto"),
-  upload.array("billsphoto", 10),
-  upload.array("cnicphoto", 2),
-  async (req, res) => {
-    const reqFilescnic = [];
+//Insert a record'
 
-    const urlcnic = req.protocol + "://" + req.get("host");
+router.post("/uploadcnic", upload.array("cnicphoto", 2), async (req, res) => {
+  const reqFiles = [];
+  const url = req.protocol + "://" + req.get("host");
 
-    for (var i = 0; i < req.files.length; i++) {
-      reqFilescnic.push(urlcnic + "/images/" + req.files[i].filename);
-    }
-    const reqFilesbill = [];
-
-    const urlbill = req.protocol + "://" + req.get("host");
-
-    for (var i = 0; i < req.files.length; i++) {
-      reqFilesbill.push(urlbill + "/images/" + req.files[i].filename);
-    }
-    let information = new Information();
-    information.userid = req.body.userid;
-    information.age = req.body.age;
-    information.income = req.body.income;
-    information.carownership = req.body.carownership;
-    information.currenthouseyears = req.body.currenthouseyears;
-    information.married = req.body.married;
-    information.profession = req.body.profession;
-    information.currentjobyears = req.body.currentjobyears;
-    information.experience = req.body.experience;
-    information.Houseownership = req.body.Houseownership;
-    information.cnic = req.body.cnic;
-    information.address = req.body.address;
-    information.amount = req.body.amount;
-    information.cnicphoto = reqFilescnic;
-    information.cardphoto = req.file.path;
-    information.billsphoto = reqFilesbill;
-    information.amount = req.body.amount;
-    information.result = req.body.result;
-    await information.save();
-    return res.send(information);
+  for (var i = 0; i < req.files.length; i++) {
+    reqFiles.push(url + "/images/" + req.files[i].filename);
   }
-);
+  let information = new Information();
+  information.userid = req.body.userid;
+  information.cnicphoto = reqFiles;
+  await information.save();
+  return res.send(information);
+});
+
+router.post("/uploadcard", upload.single("cardphoto"), async (req, res) => {
+  let information = new Information();
+  information.userid = req.body.userid;
+  information.cardphoto = req.file.path;
+  await information.save();
+  return res.send(information);
+});
+
+router.post("/", upload.array("billsphoto", 10), async (req, res) => {
+  const reqFiles = [];
+  const url = req.protocol + "://" + req.get("host");
+
+  for (var i = 0; i < req.files.length; i++) {
+    reqFiles.push(url + "/images/" + req.files[i].filename);
+  }
+  let information = new Information();
+  information.userid = req.body.userid;
+  information.age = req.body.age;
+  information.income = req.body.income;
+  information.carownership = req.body.carownership;
+  information.currenthouseyears = req.body.currenthouseyears;
+  information.married = req.body.married;
+  information.profession = req.body.profession;
+  information.currentjobyears = req.body.currentjobyears;
+  information.experience = req.body.experience;
+  information.Houseownership = req.body.Houseownership;
+  information.cnic = req.body.cnic;
+  information.address = req.body.address;
+  information.amount = req.body.amount;
+  information.billsphoto = reqFiles;
+  information.amount = req.body.amount;
+  information.result = req.body.result;
+
+  await information.save();
+  return res.send(information);
+});
 
 router.post("/predict", async (req, res) => {
   await axios
