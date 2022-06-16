8d9bb1cf7a0841c8d47ab607e2ea4697adcf4fc2
Abeer Ahmed <abeerahmad389@gmail.com>
2022-06-16 20:04:07 +0500

    code removed

--- models/user.js
+++ models/user.js
@@ -17,10 +17,7 @@ var userSchema = mongoose.Schema(
       type: String,
     },
     role: String,
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
-    verifyemailToken: String,
-    verifyemailExpire: Date,
+
     verified: {
       type: Boolean,
       default: false,
@@ -39,52 +36,6 @@ userSchema.pre("save", async function (next) {
   let salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
-
-//generate the token
-userSchema.methods.generateToken = function () {
-  return jwt.sign(
-    { _id: this._id, firstname: this.firstname },
-    process.env.JWT_KEY
-  );
-};
-
-//Generate the Reset Password Token
-userSchema.methods.getResetPasswordToken = function () {
-  //Generate the Reset Token
-  const resetToken = crypto.randomBytes(20).toString("hex");
-
-  //Hash the Above Reset Token add the resetpasswordtoken to user Schema
-  this.resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-
-  //Assign the resetpasswordToken Expire Time
-  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
-  return resetToken;
-};
-
-//Generate the email verify Token
-userSchema.methods.getverifyemailToken = function () {
-  //Generate the Reset Token
-  const emailverifyToken = crypto.randomBytes(20).toString("hex");
-
-  //Hash the Above Reset Token add the verifyemailToken to user Schema
-  this.verifyemailToken = crypto
-    .createHash("sha256")
-    .update(emailverifyToken)
-    .digest("hex");
-
-  //Assign the emailverifyToken Expire Time
-  this.verifyemailExpire = Date.now() + 60 * 1000;
-  return emailverifyToken;
-};
-function validateUserReset(data) {
-  const schema = Joi.object({
-    password: Joi.string().min(5).max(10).required(),
-  });
-  return schema.validate(data, { abortEarly: false });
-}
 
 var User = mongoose.model("User", userSchema);
 function validateUser(data) {
@@ -105,4 +56,4 @@ function validateUserLogin(data) {
   return schema.validate(data, { abortEarly: false });
 }
 
-module.exports = { User, validateUser, validateUserLogin, validateUserReset };
+module.exports = { User, validateUser, validateUserLogin };
